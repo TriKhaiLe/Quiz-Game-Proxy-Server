b6373a280b9b19d9aeefec842fc147b8c303b2b0
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-07-01 08:40:22 +0700

    enable swagger for production

--- QuizGameServer/Program.cs
+++ QuizGameServer/Program.cs
@@ -52,12 +52,8 @@ namespace QuizGameServer
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
